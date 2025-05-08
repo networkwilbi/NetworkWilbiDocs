cf56a3a2cf13ccac1aa6a97498840049f5d9718b
TrueBankai416 <carrigan@lunekes.com>
2025-05-08 14:43:01 -0400

    Add theme settings for Color mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,6 +75,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'blue-shield-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
       title: 'NetworkWilbiDocs',
       logo: {
